739021592415745f3dbedef25c52ef76cb02b8a1
ipetrash <gil9red@gmail.com>
2016-08-03 14:41:30 +0500

    Добавлена функция получения корневых элементов схемы xsd. При генерации схемы по умолчанию берется первый элемент. Усложнена схема xsd.

--- src/xsd2xml_example/Xsd2xml_example.java
+++ src/xsd2xml_example/Xsd2xml_example.java
@@ -19,14 +19,47 @@ import org.apache.xerces.xs.XSModel;
  * @author ipetrash
  */
 public class Xsd2xml_example {
+    public static 
+    java.util.List<javax.xml.namespace.QName> getGlobalElements(URL path) throws Exception {
+        // http://xmlbeans.apache.org/docs/2.4.0/reference/org/apache/xmlbeans/SchemaTypeSystem.html
+        org.apache.xmlbeans.SchemaTypeSystem sts = org.apache.xmlbeans.XmlBeans.compileXsd(
+            new org.apache.xmlbeans.XmlObject[] { 
+                org.apache.xmlbeans.XmlObject.Factory.parse(path) 
+            },
+            org.apache.xmlbeans.XmlBeans.getBuiltinTypeSystem(),
+            null
+        );
+        
+        java.util.List<javax.xml.namespace.QName> qnames = new java.util.ArrayList();
+        for (org.apache.xmlbeans.SchemaGlobalElement el: sts.globalElements()) {
+            qnames.add(el.getName());
+        }
+        
+        return qnames;
+    }
+    
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws Exception {
+    public static 
+    void main(String[] args) throws Exception {
         URL url = Xsd2xml_example.class.getResource("/resources/XmlSchema.xsd");
         String path = url.getFile();
         System.out.println("Xsd path: " + path);
-                
+
+        java.util.List<javax.xml.namespace.QName> globalElements = getGlobalElements(url);
+        if (globalElements.isEmpty()) {
+            throw new Exception("В схеме корневые элементы не были найдены.");
+        }
+        System.out.println("Корневых элементов: " + globalElements.size());
+        for (javax.xml.namespace.QName root: globalElements) {
+            System.out.println("  " + root);
+        }
+        
+        // TODO: должны быть на выбор, первый попавшийся -- пока временное решение
+        javax.xml.namespace.QName rootElement = globalElements.iterator().next();
+        System.out.println("Первый корневой элемент: " + rootElement);
+        
         XSModel xsModel = new XSParser().parse(path);
         XSInstance xsInstance = new XSInstance();
         xsInstance.generateAllChoices = Boolean.TRUE;
@@ -34,9 +67,7 @@ public class Xsd2xml_example {
         xsInstance.generateOptionalAttributes = Boolean.TRUE;
         xsInstance.generateFixedAttributes = Boolean.TRUE;
         xsInstance.generateDefaultAttributes = Boolean.TRUE;
-        
-        QName rootElement = new QName("root");
-        
+
         // Create a StringWriter for the output
         java.io.StringWriter outWriter = new java.io.StringWriter();
         XMLDocument sampleXml = new XMLDocument(new StreamResult(outWriter), true, 4, null);
